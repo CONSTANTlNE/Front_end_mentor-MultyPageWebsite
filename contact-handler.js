e853f8f197c26f73cf58bb907281b4e4e2658431
Constantine Alaverdashvili <gmta.constantine@gmail.com>
2023-07-18 15:19:39 +0400

    just update

--- contact-handler.js
+++ contact-handler.js
@@ -7,32 +7,35 @@ const hidden = document.querySelectorAll(".hidden");
 const input = document.querySelectorAll(".input");
 const inc_email = document.querySelector(".incorrect_mail");
 
-
 submit_btn.addEventListener("click", (e) => {
+  // validation for empty fields, shows hidden notifications
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].value === "") {
+      e.preventDefault();
+      hidden[i].style.display = "block";
+    }
+  }
+
+  function isValidEmail(email) {
+    // Regular expression pattern for validating email addresses
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // Test the email address against the pattern and return the result
+
+    return pattern.test(email);
+  }
+
+  // display error if email is invalid
+  if (!email.value == "" && !isValidEmail(email.value)) {
+    inc_email.style.display = "block";
     e.preventDefault();
-    // validation for empty fields, shows hidden notifications
-    for (let i = 0; i < input.length; i++) {
-        if (input[i].value === "") {
-            hidden[i].style.display = "block";
-        }
-    }
-
-    function isValidEmail(mail) {
-        // Regular expression pattern for validating email addresses
-        const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        // Test the email address against the pattern and return the result
-        return pattern.test(mail);
-    }
-
-    // hide error notifications if filled
-    if (!email.value == "" && !isValidEmail(email.value)) {
-        inc_email.style.display = "block";
-    }
-    input.forEach((inc_email) => {
-        inc_email.addEventListener("focus", () => {
-            hidden.forEach((inc_email) => {
-                inc_email.style.display = "none";
-            });
-        });
+  }
+  // when clicking inside input field hide error notif.
+  input.forEach((a) => {
+    a.addEventListener("focus", () => {
+      inc_email.style.display = "none";
+      hidden.forEach((b) => {
+        b.style.display = "none";
+      });
     });
+  });
 });
